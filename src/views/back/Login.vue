219a8f48789cc257c6ba69a921fc7b60355ba7cc
linancemelo <ilampili@yahoo.com.tw>
2023-05-07 21:09:01 +0800

    no message

--- src/views/back/Login.vue
+++ src/views/back/Login.vue
@@ -1,51 +1,60 @@
 <template>
-  <div class="login-container">
-    <div class="row py-5">
-      <div class="col-md-6 text-center">
-        <img src="@/assets/logo.png" alt="" class="img-fluid h-100" />
-      </div>
-      <div class="col-md-6 d-flex flex-column justify-content-center py-3 px-lg-6">
-        <h3 class="mb-5 text-center">登入後台</h3>
-        <form @submit.prevent="login">
-          <div class="mb-3">
-            <label for="inputEmail">帳號</label>
-            <input
-              type="email"
-              id="inputEmail"
-              class="form-control"
-              placeholder="Email address"
-              required
-              v-model="userInfo.username"
-            />
-          </div>
-          <div class="mb-3">
-            <label for="inputPassword">密碼</label>
-            <input
-              type="password"
-              id="inputPassword"
-              class="form-control"
-              placeholder="Password"
-              required
-              v-model="userInfo.password"
-            />
-          </div>
-          <div class="mb-4">
-            <input type="checkbox" id="rememberAc" />
-            <label for="rememberAc">記住我的帳號</label>
-          </div>
-          <div class="text-center">
-            <button class="btn btn-dark btn-md w-100" type="submit">
-              登入
-            </button>
-          </div>
-        </form>
-        <div class="text-center my-4">
-          <span class="dash"></span>
-          <span class="text-secondary">或</span>
-          <span class="dash"></span>
+  <div class="background">
+    <div class="login-container">
+      <div class="row py-md-5 bg-white">
+        <div class="col-md-6 d-flex justify-content-center align-items-center">
+          <img src="../../assets/img/logo.png" alt="" class="img-fluid" />
         </div>
-        <div class="text-center">
-          <a class="link-secondary">忘記密碼？</a>
+        <div
+          class="col-md-6 d-flex flex-column justify-content-center py-md-3 px-lg-6"
+        >
+          <h3 class="mb-md-5 text-center">登入後台</h3>
+          <form @submit.prevent="login">
+            <div class="mb-3 fs-7">
+              <label for="inputEmail">帳號</label>
+              <input
+                type="email"
+                id="inputEmail"
+                class="form-control"
+                placeholder="Email address"
+                required
+                v-model="userInfo.username"
+              />
+            </div>
+            <div class="mb-2 mb-md-3">
+              <label for="inputPassword" class="fs-7">密碼</label>
+              <input
+                type="password"
+                id="inputPassword"
+                class="form-control"
+                placeholder="Password"
+                required
+                v-model="userInfo.password"
+              />
+            </div>
+            <div class="mb-3 mb-md-4">
+              <input
+                type="checkbox"
+                id="rememberAc"
+                class="me-2"
+                v-model="rememberAc"
+              />
+              <label for="rememberAc" class="fs-7">記住我的帳號</label>
+            </div>
+            <div class="text-center">
+              <button class="btn btn-dark btn-md w-100" type="submit">
+                登入
+              </button>
+            </div>
+          </form>
+          <div class="text-center my-2 my-md-4">
+            <span class="dash"></span>
+            <span class="text-secondary">或</span>
+            <span class="dash"></span>
+          </div>
+          <div class="text-center mb-4">
+            <a class="link-secondary">忘記密碼？</a>
+          </div>
         </div>
       </div>
     </div>
@@ -53,32 +62,61 @@
 </template>
 
 <script setup>
-import { callApi, simpleAlert, setCookie } from "@/composables/units";
+import {
+  callApi,
+  simpleAlert,
+  setCookie,
+  getCookie,
+  removeCookie
+} from "@/composables/units";
 import { useRouter } from "vue-router";
 const router = useRouter();
 
 const userInfo = ref({
-  username: "liangvuepractice@mail.com",
+  username: "",
   password: "liangxu04vu6",
 });
+const rememberAc = ref(false);
 // 登入
 const login = () => {
   callApi(`admin/signin`, "POST", userInfo.value)
-    .then(async (result) => {
+    .then(async(result) => {
       if (result.data.success) {
         setCookie("ltk", result.data.token, 0);
+        setCookie("linanceAc", userInfo.value.username, 7);
+        if (rememberAc.value) {
+            setCookie("rememberAc", rememberAc.value, 7);
+        } else {
+            removeCookie("rememberAc");
+            removeCookie("linanceAc");
+        }
         await simpleAlert("登入成功", "success");
         router.push({ name: "Manage" });
+      } else {
+          simpleAlert(`${result.data.message}`, "error");
+          return false;
       }
     })
     .catch((err) => console.log(err));
 };
+
+onMounted(() => {
+  if (getCookie("rememberAc") && getCookie("linanceAc")) {
+      rememberAc.value = getCookie("rememberAc");
+      userInfo.value.username = getCookie("linanceAc");
+  }
+});
 </script>
 
 <style scoped lang="scss">
+.background {
+  background-image: url("@/assets/img/login_bg.jpeg");
+  display: flex;
+  place-items: center;
+  overflow: hidden;
+}
 .login-container {
-  padding-left: 10rem;
-  padding-right: 10rem;
+  padding: 5rem 10rem;
 }
 .row {
   box-shadow: 0 0 20px;
@@ -90,7 +128,11 @@ const login = () => {
     vertical-align: middle;
   }
 }
-
+@media (min-width: 768px) {
+  .background {
+    height: 100vh;
+  }
+}
 @media (max-width: 1200px) {
   .login-container {
     padding-left: 1.5rem;
