da632c41c7c4ff6829123ddcfb0122117cfe3ec5
ELAND\hungliangli <hungliangli@eland.com.tw>
2023-07-03 17:37:26 +0800

    no message

--- src/main.js
+++ src/main.js
@@ -4,8 +4,6 @@ import router from "@/router/index";
 import { createPinia } from "pinia";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "@popperjs/core";
-import "@/assets/main.css";
-import "@/assets/style.scss";
 import "vue-loading-overlay/dist/css/index.css";
 
 const app = createApp(App);
