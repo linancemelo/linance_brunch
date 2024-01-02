69008d01135457eda2c42ecd4ec6229f935df976
DESKTOP-1OQE5RI\User <k9789822@gmail.com>
2024-01-02 21:38:14 +0800

    no message

--- deploy.sh
+++ deploy.sh
@@ -9,9 +9,6 @@ npm run build
 # 移動到打包資料夾下，若你有調整的話打包後的資料夾請務必調整
 cd dist
 
-# 部署到自定義網域
-# echo 'www.example.com' > CNAME
-
 git init
 git add -A
 git commit -m 'deploy'
@@ -22,7 +19,7 @@ git commit -m 'deploy'
 # 部署到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
 # 以這個專案來講就要改成這樣以下這樣，下面是走 ssh 模式
-git push -f git@github.com:linancemelo/linance_brunch.git master:gh-pages
+git push -f git@github.com:linancemelo/linance_brunch.git main:gh-pages
 # 除此之外，也可以改走 HTTPS 模式
 # git push -f https://github.com/hsiangfeng/HexfootMusic.git master:gh-pages
 
