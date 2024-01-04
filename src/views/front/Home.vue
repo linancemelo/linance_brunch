0b30db0d995775d592513ed7f3bb75bb1da3882e
linancemelo <ilampili@yahoo.com.tw>
2024-01-04 22:39:27 +0800

    > kill ScrollTrigger

--- src/views/front/Home.vue
+++ src/views/front/Home.vue
@@ -101,6 +101,10 @@ onMounted(() => {
   });
 });
 
+onUnmounted(() =>  {
+  ScrollTrigger.killAll();
+})
+
 const getImageUrl = (url) => {
   return new URL(`/assets/img/${url}`, import.meta.url).href;
 };
